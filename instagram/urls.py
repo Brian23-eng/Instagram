13b1002e87a15f2f66fa4acd581ae162c2b68038
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-16 12:55:23 +0300

    create a registration form

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
     
     url('^$', views.home, name='home'),
-    url('^login/', views.login, name ='login'),
+    
     
     
     
--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from . models import Images, Comments, Profile
 
 # Create your views here.
-
+@login_required(login_url='/accounts/register/')
 def home(request):
-    return render(request,'landing-page/home.html')
+    return render(request,'home.html')
 
-
-def login(request):
-    return render(request, 'registration/login.html')
