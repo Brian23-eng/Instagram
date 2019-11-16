f185b7cd5d8d7ca001d2c2ef4bb624d278e9c72c
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-16 17:04:24 +0300

    create a template that shows users profile

--- insta/urls.py
+++ insta/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('instagram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    # url(r'^login/$',views.login, {"next_page": ''}),
+    url(r'^logout/$',views.logout, {"next_page": '/'}),
     url(r'^tinymce/', include('tinymce.urls')),
     
 ]
