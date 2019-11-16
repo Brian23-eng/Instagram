e6c17f50a090e132131a20e376a9565833aa21bd
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-16 14:58:42 +0300

    create users profile that can be accessed using users id

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     
     url('^$', views.home, name='home'), 
+    url(r'^user/(?P<username>\w+)',views.profile,name='profile'),
 ]
 
 
--- instagram/views.py
+++ instagram/views.py
@@ -11,3 +11,11 @@ def home(request):
     images = Images.get_all_images()
     return render(request,'home.html',{'images':images})
 
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+    user = User.objects.get(username=username)
+    profile = Profiles.filter_profile_by_id(user.id)
+    title = f'{user.username}\'s Profile '
+    images = Images.get_profile_images(user.id)
+    return render(request, 'profile/profile.html',{'title':title,'users':user,'profile':profile,'images':images})
