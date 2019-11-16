6996158ccb7490cd78ad844aebc2cff98489e035
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-16 12:34:24 +0300

    add comment class for comments

--- instagram/models.py
+++ instagram/models.py
@@ -16,3 +16,12 @@ class Images(models.Model):
     posted = models.DateTimeField(auto_now=True)
     profile = models.ForeignKey(User,on_delete=models.CASCADE)
     
+    
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length=100)
+    posted = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Images,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    
