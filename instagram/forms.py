2b9cbcc7d315fd80f01edc58cce436c2ad9d1b34
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-16 14:30:36 +0300

    create a post image form class the will enable users upload photos and create comments and even update their profile

---
+++ instagram/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Profiles Comments, Images
+
+class PostImagesForm(forms.ModelForm):
+    image = forms.ImageField()
+    caption = forms.CharField(max_length=100)
+    class Meta: 
+        model = Images
+        exclude = ['posted','profile']
+        
+        
+class PostComment(forms.ModelForm):
+    class Meta: 
+        model = Comments
+        exclude = ['image','posted','user']
+
+class PostProfile(forms.ModelForm):
+    class Meta:
+        models = Profiles
+        exclude = ['user',]
+        
